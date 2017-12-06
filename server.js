6fd5b8f7afe6aa321c5adb4015caa82218d7f2b5
clement <chankinmingclement@gmail.com>
2017-12-06 14:41:25 +0800

    first commit

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-'use strict';
+var express = require('express');
+var socket = require('socket.io');
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+var app = express();
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+const port = process.env.PORT || 5000;
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const io = socketIO(server);
-
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+server = app.listen(port, function(){
+    console.log('server is running on port 5000')
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+
+    socket.on('SEND_MESSAGE', function(data){
+        io.emit('RECEIVE_MESSAGE', data);
+    })
+});
