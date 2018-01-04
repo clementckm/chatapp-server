d4e6c2571dfefb3aa150a96b508f2300fff3c4f2
clement <chankinmingclement@gmail.com>
2018-01-04 16:49:10 +0800

    fix the chat history and friend list

--- server.js
+++ server.js
@@ -34,10 +34,42 @@ var privateChat = [];
 function addPrivateMessage (data){
   privateChat.push(data)
 }
+
+var sortPrivateChat = [];
+function sortChat(){
+  var len
+  var p = privateChat.slice()
+  for(var i = 0, len = privateChat.length; i < len; i++ ) {
+       if( (p[i].receiver === '0x7f4f06c8962ca1fce3abb6ee63f5c9d7e50b8506' && p[i].sender === '0xbe71777c4f0c44c5d4f3c9d482ae1733095a1640') || (p[i].sender === '0x7f4f06c8962ca1fce3abb6ee63f5c9d7e50b8506' && p[i].receiver === '0xbe71777c4f0c44c5d4f3c9d482ae1733095a1640')){
+           sortPrivateChat.push(p[i])
+       }
+   }
+}
+
+function removeChat() {
+  sortPrivateChat = [];
+}
+
 // Friends
 var friends = [];
+
 function addFriend (data) {
   friends.push(data)
+}
+
+
+var sortFriends = [];
+function sortFriend (data) {
+  var len
+  var f = friends.slice()
+  for(var i = 0, len = friends.length; i < len; i++ ) {
+       if( f[i].userAddress === data ){
+           sortFriends.push(f[i].friend)
+       }
+   }
+}
+function removeFriend () {
+  sortFriends = [];
 }
 
 
@@ -50,30 +82,36 @@ io.on('connection', (socket) => {
   // User
     socket.on('ONLINE', function(data){
         onlineUser.push(data.userAddress);
-        console.log(onlineUser)
         io.emit('ONLINE_USER', onlineUser);
     })
     socket.on('OFFLINE', function(data){
         removeUser(data);
-        console.log(onlineUser)
         io.emit('OFFLINE_USER', onlineUser);
     })
   // Private Channel
     socket.on('JOIN', function(room) {
       console.log('room hash:', room)
       socket.join(room);
-      console.log('join room')
+      console.log('join privateChat')
     });
     socket.on('PRIVATE_MESSAGE', function(data) {
+      console.log(data)
       // io.sockets.to(data.roomSender).emit('PRIVATE_MESSAGE', data);
       addPrivateMessage(data)
+      // sortChat()
+      // console.log('sortPrivateChat', sortPrivateChat)
       io.sockets.to(data.roomReceiver).emit('PRIVATE_MESSAGE', privateChat);
+      socket.leave(data.roomReceiver)
     });
   // Friend List
+    socket.on('JOIN_SELF', function(room) {
+      socket.join(room);
+    });
     socket.on('FRIENDS', function(data){
-        console.log(data)
         addFriend(data);
-        console.log(friends)
-        io.emit('FRIENDS_LIST', friends);
+        sortFriend(data.userAddress)
+        io.sockets.to(data.userAddress).emit('FRIENDS_LIST', sortFriends);
+        removeFriend();
+        socket.leave(data.userAddress);
     })
 });
